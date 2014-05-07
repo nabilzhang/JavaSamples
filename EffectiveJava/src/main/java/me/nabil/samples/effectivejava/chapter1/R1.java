d123d3b2b74848d818114e587cd47b3cbd713544
zhangbi <dumuqiao0317@gmail.com>
2014-05-07 21:26:02 +0800

    comment

--- EffectiveJava/src/main/java/me/nabil/samples/effectivejava/chapter1/R1.java
+++ EffectiveJava/src/main/java/me/nabil/samples/effectivejava/chapter1/R1.java
@@ -15,19 +15,26 @@ public class R1 {
             IllegalAccessException {
         R1Temp r1Temp = R1Temp.class.newInstance();
         r1Temp.sayHello();
-
+        /**
+         * ---------------------------------------
+         */
         // 静态工厂方法引用同一对象
         System.out.println(Boolean.valueOf(true) == Boolean.valueOf(true));
 
         // 不同对象
         System.out.println(new Boolean(true) == new Boolean(true));
 
-        // 静态工厂与构造器不同的第三大优势在于,他们可以返回原返回类型的任何子类型的对象
+        /**
+         * ---------------------------------------
+         * 静态工厂与构造器不同的第三大优势在于,他们可以返回原返回类型的任何子类型的对象
+         */
+        //
         EnumSet<Enuma> set = EnumSet.noneOf(Enuma.class);
         System.out.println(set);
 
         set = EnumSet.allOf(Enuma.class);
         System.out.println(set);
+
     }
 }
 

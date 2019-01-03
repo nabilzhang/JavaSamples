9ed107f5762c0c018d91091adbd3ccb9dc711012
nabilzhang <dumuqiao0317@qq.com>
2019-01-03 17:55:45 +0800

    netty 4

---
+++ Netty4Demo/src/me/nabil/demo/nettydemo/UnixTime.java
@@ -0,0 +1,30 @@
+package me.nabil.demo.nettydemo;
+
+import java.util.Date;
+
+/**
+ * UnixTime
+ *
+ * @author nabil
+ * @date 2019/1/3
+ */
+public class UnixTime {
+    private final long value;
+
+    public UnixTime() {
+        this(System.currentTimeMillis() / 1000L + 2208988800L);
+    }
+
+    public UnixTime(long value) {
+        this.value = value;
+    }
+
+    public long value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return new Date((value() - 2208988800L) * 1000L).toString();
+    }
+}

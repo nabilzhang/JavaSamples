50a8948c1be04406eead371e56a4dbad0bf90584
nabilzhang <dumuqiao0317@qq.com>
2019-03-13 17:58:57 +0800

    aspectj

--- SpringBootDemo/SpringBootAspectjDemo/src/main/java/me/nabil/demo/aspectj/Application.java
+++ SpringBootDemo/SpringBootAspectjDemo/src/main/java/me/nabil/demo/aspectj/Application.java
@@ -3,7 +3,6 @@ package me.nabil.demo.aspectj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AdviceMode;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -14,8 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
-@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
+@EnableTransactionManagement(mode = AdviceMode.PROXY)
 public class Application {
 
     public static void main(String[] args) {
--- SpringBootDemo/SpringBootAspectjDemo/src/main/java/me/nabil/demo/aspectj/service/impl/TransactionMgr.java
+++ SpringBootDemo/SpringBootAspectjDemo/src/main/java/me/nabil/demo/aspectj/service/impl/TransactionMgr.java
@@ -4,46 +4,24 @@ import me.nabil.demo.aspectj.domain.Order;
 import me.nabil.demo.aspectj.domain.OrderGoods;
 import me.nabil.demo.aspectj.repository.OrderGoodsRepository;
 import me.nabil.demo.aspectj.repository.OrderRepository;
-import me.nabil.demo.aspectj.service.OrderService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * order service
+ * TransactionMgr
  *
  * @author nabilzhang
  * @date 2019/3/13
  */
-@Service
-public class OrderServiceImpl implements OrderService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
+@Component
+public class TransactionMgr {
 
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
     private OrderGoodsRepository orderGoodsRepository;
-
-    @Autowired
-    private TransactionMgr transactionMgr;
-
-    @Override
-    public void generateOrder() {
-        LOGGER.info("generating order");
-        Order order = new Order();
-        order.setName("test");
-
-        OrderGoods orderGoods = new OrderGoods();
-        orderGoods.setGoodsId(1);
-
-//        saveInDb(order, orderGoods);
-        transactionMgr.saveInDb(order, orderGoods);
-        LOGGER.info("generating order finish");
-    }
 
     @Transactional
     public void saveInDb(Order order, OrderGoods orderGoods) {
